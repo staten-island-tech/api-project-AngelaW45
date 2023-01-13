9c1627bdc24807b3beef10889c710ad7787bbb7a
AngelaW45 <113141296+AngelaW45@users.noreply.github.com>
2023-01-13 10:15:47 -0500

    8

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,12 @@
 import "./style.css";
-const characters =
-  "https://raw.githubusercontent.com/genshindev/api/mistress/assets/data/characters/ayaka/en.json";
+const characters = "https://genshinlist.com/api/characters";
+
+const DOMSelectors = {
+  Character: document.querySelector(".c"),
+  Element: document.querySelector(".e"),
+  WeaponType: document.querySelector(".w"),
+  displaySection: document.getElementById("display"),
+};
 
 async function getData(characters) {
   try {
@@ -9,7 +15,17 @@ async function getData(characters) {
       throw new Error(response);
     } else {
       const data = await response.json();
-      document.getElementById("api-response-1").textContent = data.characters; DOES NOT WORK
+      document.getElementById("api-response-1").textContent = data.name;
+      document.getElementById("api-response-2").textContent = data.vision;
+      document.getElementById("api-response-3").textContent = data.weapon;
+
+      function all() {
+        data.forEach((character) => {
+          card(character);
+        });
+      }
+      all();
+
       console.log(response.status);
       console.log(data);
     }
@@ -19,27 +35,50 @@ async function getData(characters) {
   }
 }
 getData(characters);
+``;
 
-const DOMSelectors = {
-  Character: document.querySelector(".c"),
-  Element: document.querySelector(".e"),
-  WeaponType: document.querySelector(".w"),
-};
+function card(character) {
+  DOMSelectors.displaySection.insertAdjacentHTML(
+    "beforeend",
+    `<div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <h3 class="display-name"> ${character.name} </h3>
+            <h4 class="display-type"> ${character.vision} </h4>
+            <h4 class="display-weapon">${character.weapon} </h4>
+        </div>
+        <div class="flip-card-back">
+          <h3 class="display-discription"> ${character.discription}</h3>
+            <h3 class="display-birthday"> ${character.birthday} </h3>
+            <h4 class="display-gender"> ${character.gender} </h3>
+            <h4 class="displpay-rarity">${character.rarity} </h4>
+        </div>
+      </div>
+    </div>`
+  );
+}
 
-/* const URL = "https://api.genshin.dev/"; */
+/* const characters =
+  "https://raw.githubusercontent.com/genshindev/api/mistress/assets/data/characters/ayaka/en.json";
 
-/* async function getData(URL) {
+async function getData(characters) {
   try {
-    const response = await fetch(URL);
+    const response = await fetch(characters);
     if (response.status < 200 || response.status > 299) {
       throw new Error(response);
     } else {
       const data = await response.json();
+      document.getElementById("api-response-1").textContent = data.name;
+      document.getElementById("api-response-2").textContent = data.vision;
+      document.getElementById("api-response-3").textContent = data.weapon;
       console.log(response.status);
       console.log(data);
     }
   } catch (error) {
     console.log(error);
-    console.log("The status is above 299, something went wrong");
+    console.log("Could not find, please try again");
   }
-  getData(URL); */
+}
+getData(characters);
+``;
+ */
